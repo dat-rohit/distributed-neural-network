f6677e278420925a923caa2106e47f649b658bf3
rohitkumar.datchanamourty <rdatchane.19@gmail.com>
2023-11-30 14:40:52 +0900

    defined network

--- models/model.py
+++ models/model.py
@@ -1,1 +1,37 @@
-print("ok")
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.optim as optim
+
+
+
+
+class Network(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.convolutions = nn.Sequential(
+             nn.Conv2d(3, 6, 5),
+             nn.MaxPool2d(2, 2),
+             nn.Conv2d(6, 16, 5),
+             nn.MaxPool2d(2, 2))
+        
+        self.fc = nn.Sequential(
+            nn.Linear(6*16*5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10))
+        
+    def forward(self, x):
+
+        x = self.convolutions(x)
+        x = self.fc(x)
+
+        return x
+    
+
+
+    
+
---
+++ train.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+import numpy as np
+import os 
+import argparse
+
+
+from models.model import Network
