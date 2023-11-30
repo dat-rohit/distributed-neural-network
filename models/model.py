dd37a4aceade624501aa52382b52f7a4e84eccd8
rohitkumar.datchanamourty <rdatchane.19@gmail.com>
2023-11-30 16:30:07 +0900

    training code

--- models/model.py
+++ models/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch.optim as optim
-
+import torch.nn.functional as F
 
 
 
@@ -10,26 +10,22 @@ class Network(nn.Module):
 
     def __init__(self):
         super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-        self.convolutions = nn.Sequential(
-             nn.Conv2d(3, 6, 5),
-             nn.MaxPool2d(2, 2),
-             nn.Conv2d(6, 16, 5),
-             nn.MaxPool2d(2, 2))
-        
-        self.fc = nn.Sequential(
-            nn.Linear(6*16*5, 120),
-            nn.ReLU(),
-            nn.Linear(120, 84),
-            nn.ReLU(),
-            nn.Linear(84, 10))
-        
     def forward(self, x):
-
-        x = self.convolutions(x)
-        x = self.fc(x)
-
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
+
     
 
 
--- train.py
+++ train.py
@@ -4,9 +4,66 @@ import torch.nn.init as init
 import torch.optim as optim
 import torch.utils.data as data
 import torchvision
+import torchvision.transforms as transforms
 import numpy as np
 import os 
 import argparse
 
 
 from models.model import Network
+
+
+
+
+if __name__ == '__main__':    
+
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+
+    train = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    test = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(test, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+
+    model = Network()
+    model.to(device=device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
