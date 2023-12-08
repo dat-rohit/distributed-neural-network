07a5bc7dcfdad01b26c2eb9cc5b29275b52cadb3
dat-rohit <rdatchane.19@gmail.com>
2023-12-08 13:17:10 +0900

    mpi only implementation

---
+++ bis_distributed_train.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.distributed as dist
+from torch.multiprocessing import Process
+import torchvision
+import torchvision.transforms as transforms
+
+# MPI library:
+from mpi4py import MPI
+
+# convnet model that classifies MNIST:
+from models.model import Network
+
+# other utilities:
+import numpy as np
+import os
+import argparse
+from collections import OrderedDict
+
+
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+criterion = nn.CrossEntropyLoss()
+
+
+def eval(model):
+
+    model.eval()
+    with torch.no_grad():
+        testLoader = data.DataLoader(
+        torchvision.datasets.CIFAR10(root='./data', train=False, download=True, batch_size=args.bs, transform=transform))
+        losses=[]
+        for (input, target) in enumerate(testLoader):
+            loss=criterion(model(input), target)
+            losses.append(loss.item())
+
+def run_child(model, comm, args):
+
+    trainLoader = data.DataLoader(
+        torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform),
+        shuffle=True,
+        num_workers=1,
+        batch_size=args.bs
+    )
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    
+    for i, (input, target) in enumerate(trainLoader):
+
+        optimizer.zero_grad()
+        output=model(input)
+        loss=criterion(output, target)
+        loss.backward()
+        optimizer.step()
+    comm.send(model.state_dict(), 0)
+
+#TODO code run_parent anb main function
+#Code gradient upload and parameter synchronization
+#Code dataset partitioner
+
+
+def run_parent(model, comm, size, args):
+    state_dicts = []
+
+
+    #receive the model parameters of all childrem process
+    for p in range(size-1):
+        state_dicts.append(comm.recv())
+
+    #perform Parameter Synchronization
+    avg_state_dict = OrderedDict()
+    for key in state_dicts[0].keys():
+        avg_state_dict[key] = sum([sd[key] for sd in state_dicts]) / float(size-1)
+    
+    #update the master model with the synchronized parameters
+    model.load_state_dict(avg_state_dict)
+
+def main(args):
+
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    model = Network()
+
+    if rank == 0:
+
+        for i in range(args.epochs):
+            comm.send(model.state_dict(), k) for k in
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr", dest="lr", default=0.001)
+    parser.add_argument("--momentum", dest="momentum", default=0.9)
+    parser.add_argument("--batch-size", dest="bs", default=16)
+    parser.add_argument("--epochs", dest="epochs", default=10)
+    args = parser.parse_args()
